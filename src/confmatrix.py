27c41b33de99f09f4b4192aaf5cf4f336e21c0ef
clara348 <assoulineclara@gmail.com>
2022-06-07 12:36:54 -0400

    maybe fixed bugs in confusion matrix

--- src/confmatrix.py
+++ src/confmatrix.py
@@ -12,6 +12,7 @@ from sklearn.metrics import confusion_matrix, classification_report
 
 modelB = training.CNN
 modelB.load_state_dict(torch.load('saved_model'), strict=False)
+_, (train_dataset, test_dataset) = preprocessing.pre_processing('../resized_images')
 
 torch.manual_seed(0)
 net = NeuralNetClassifier(
@@ -27,21 +28,21 @@ net = NeuralNetClassifier(
     criterion=nn.CrossEntropyLoss,
     # device=DEVICE
 )
-(train_loader, test_loader), (train_dataset, test_dataset) = preprocessing.pre_processing('../resized_images')
-data_len = len(train_dataset)
-split_train, val_data = random_split(train_dataset, [int(data_len - data_len * 0.2), int(data_len * 0.2)])
-y_train = np.array([y for x, y in iter(split_train)])
-
-net.fit(split_train, y=y_train)
-y_pred = net.predict(test_dataset)
-y_true = np.array([y for x, y in iter(test_dataset)])
-#accuracy_score(y_true, y_pred)
-target_names = preprocessing.get_classes()
-class_rep = classification_report(y_true, y_pred, target_names=target_names)
-confusion_matrix(y_true,y_pred,labels=target_names)
-print("Classification Report ", class_rep)
 
 
+# data_len = len(train_dataset)
+# split_train, val_data = random_split(train_dataset, [int(data_len - data_len * 0.2), int(data_len * 0.2)])
+
+def evaluation():
+    y_train = np.array([y for x, y in iter(train_dataset)])
+    net.fit(train_dataset, y=y_train)
+    y_pred = net.predict(test_dataset)
+    y_true = np.array([y for x, y in iter(test_dataset)])
+    # accuracy_score(y_true, y_pred)
+    target_names = preprocessing.get_classes()
+    class_rep = classification_report(y_true, y_pred, target_names=target_names)
+    confusion_matrix(y_true, y_pred, labels=target_names)
+    print("Classification Report ", class_rep)
 
 # y_test = np.array([y for x, y in iter(testset)]) accuracy_score(y_test, y_pred)
 # plot_confusion_matrix(net, testset, y_test.reshape(-1, 1)) plt.show()
