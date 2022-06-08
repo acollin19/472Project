1f80aa0aaf7a3d7cbf36604e6f860a3c6cf8f120
clara348 <assoulineclara@gmail.com>
2022-06-07 20:07:31 -0400

    tried old code

--- src/confmatrix.py
+++ src/confmatrix.py
@@ -26,7 +26,7 @@ net = NeuralNetClassifier(
     iterator_train__num_workers=4,
     iterator_valid__num_workers=4,
     lr=1e-3,
-    batch_size=64,
+    batch_size=128,
     optimizer=optim.Adam,
     criterion=nn.CrossEntropyLoss,
     device=DEVICE
@@ -53,5 +53,38 @@ def evaluation():
     #                          scoring="accuracy")
 
 
+# # dataset and train_dataset from preprocessing.py
+# y_train = np.array([y for x, y in iter(train_dataset)])
+# net.fit(train_dataset, y=y_train)
+#
+# y_true = np.array([y for x, y in iter(test_dataset)])
+# y_pred = net.predict(test_dataset)
+#
+#
+# # calculate accuracy, precision, f1-measure, recall given confusion matrix
+# def confusion_matrix(true, predicted):
+#     cm = confusion_matrix(true, predicted)
+#     accuracy = accuracy_score(true, predicted)
+#     f1 = f1_score(true, predicted)
+#     precision = precision_score(true, predicted)
+#     recall = recall_score(true, predicted)
+#
+#     # compiling results into a table
+#     table = [['Accuracy', accuracy],
+#              ['Precision', precision],
+#              ['Recall', recall],
+#              ['F1-measure', f1],
+#              ['Confusion Matrix', cm]]
+#     results = tabulate(table, tablefmt='fancy_grid')
+#
+#     return results
+#
+#
+# confusion_matrix(y_true, y_pred)
+
+
 if __name__ == '__main__':
     evaluation()
+    # true = y_true
+    # predicted = y_pred
+    # confusion_matrix(true, predicted)
