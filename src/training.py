d48483025b7f687cbab8149a05c523679aa70c0f
Timothee Duthoit <55003440+timotheeduthoit@users.noreply.github.com>
2022-06-08 16:35:34 -0400

    added part to run on gpu for mac

--- src/training.py
+++ src/training.py
@@ -1,6 +1,8 @@
 import sys
 import torch
 import torch.nn as nn
+from torch.backends import cudnn
+
 import preprocessing
 
 
@@ -63,13 +65,17 @@ class CNN(nn.Module):
 
 def cnn():
     # Parameters that can be tuned
-    num_epochs = 10
+    num_epochs = 40
     num_classes = 4
     learning_rate = 0.001
     loaders, _ = preprocessing.pre_processing('../resized_images')
     train_loader, test_loader = loaders
 
-    model = CNN()
+    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # For windows
+    device = torch.device('mps' if torch.has_mps else 'cpu')  # For mac
+    print("Device used to train the model: {device}".format(device=device))
+
+    model = CNN().to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
@@ -78,7 +84,8 @@ def cnn():
     acc_list = []
     for epoch in range(num_epochs):
         print('epoch {e}'.format(e=epoch))
-        for i, (images, labels) in enumerate(train_loader):
+        for i, data in enumerate(train_loader):
+            images, labels = data[0].to(device), data[1].to(device)
             # Forward pass
             outputs = model(images)
             loss = criterion(outputs, labels)
@@ -95,7 +102,7 @@ def cnn():
             correct = (predicted == labels).sum().item()
             acc_list.append(correct / total)
 
-            if (i + 1) % 100 == 0:
+            if (i + 1) % 10 == 0:
                 print(
                     'Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%'.format(epoch + 1, num_epochs, i + 1,
                                                                                           total_step, loss.item(),
@@ -104,7 +111,8 @@ def cnn():
     with torch.no_grad():
         correct = 0
         total = 0
-        for images, labels in test_loader:
+        for data in test_loader:
+            images, labels = data[0].to(device), data[1].to(device)
             outputs = model(images)
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
