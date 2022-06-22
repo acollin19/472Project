4fc6fa4756631e7798af6238503793650542b5d0
clara348 <assoulineclara@gmail.com>
2022-06-22 11:57:49 -0400

    fixed k-fold output + added means

--- src/confmatrix.py
+++ src/confmatrix.py
@@ -34,8 +34,8 @@ if X.is_mps:
     X = X.cpu()
 Also a nightly version of pytorch is required
 """
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # For windows (will use cpu on macs)
-# device = torch.device('mps' if torch.has_mps else 'cpu')  # For mac (M1 macs with nightly version of pytorch)
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # For windows (will use cpu on macs)
+device = torch.device('mps' if torch.has_mps else 'cpu')  # For mac (M1 macs with nightly version of pytorch)
 print("Device used to compute the confusion matrix: {device}".format(device=device))
 
 modelB = CNN()
@@ -69,19 +69,25 @@ def evaluation(img_path, train_data, test_data):
 def k_fold():
     k_train = np.array([y for x, y in iter(train_dataset)])
     net.fit(train_dataset, y=k_train)
+
+    # macro or weighted
     scoring = {'accuracy': make_scorer(accuracy_score),
-               'precision': make_scorer(precision_score, average='micro'),
-               'recall': make_scorer(recall_score, average='micro'),
-               'f1_score': make_scorer(f1_score, average='micro')}
+               'precision': make_scorer(precision_score, average='weighted', zero_division=1),
+               'recall': make_scorer(recall_score, average='weighted', zero_division=1),
+               'f1_score': make_scorer(f1_score, average='weighted', zero_division=1)}
     # k-fold
     train_sliceable = SliceDataset(train_dataset)
     kfold = KFold(n_splits=10, shuffle=True, random_state=10)
     # scores = cross_val_score(net, train_sliceable, k_train, cv=10, scoring="accuracy")
-    # scores = cross_val_score(net, train_sliceable, k_train, cv=2, scoring=scoring)
-    # avg_scores = scores.mean()
+    # scores = cross_val_score(net, train_sliceable, k_train, cv=kfold, scoring=scoring)
     scores = cross_validate(net, train_sliceable, k_train, cv=kfold, scoring=scoring)
-
     print("All scores ", scores)
+
+    # aggregate values ?
+    print("Accuracy Mean ", (np.mean(scores['test_accuracy']))*100)
+    print("Precision Mean", (np.mean(scores['test_precision']))*100)
+    print("Recall Mean", (np.mean(scores['test_recall']))*100)
+    print("F1 Mean", (np.mean(scores['test_f1_score']))*100)
 
 
 if __name__ == '__main__':
